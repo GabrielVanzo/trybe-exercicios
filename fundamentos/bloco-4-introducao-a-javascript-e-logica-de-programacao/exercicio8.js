38dc8adfc6f2859f3c3f6939ac240561a2d717b8
GabrielVanzo <gabriel.vanzo98@gmail.com>
2022-08-18 21:42:25 -0300

    Conclusao 8

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/exercicio8.js
@@ -0,0 +1,12 @@
+// EXERCICIO 8
+
+const a = 3
+const b = 5
+const c = 7
+const sum = a + b + c;
+
+if (a % 2 === 0 || b % 2 === 0 || c % 2 === 0) {
+    console.log(true);
+} else {
+    console.log(false);
+}
