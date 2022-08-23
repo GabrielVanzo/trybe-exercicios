97934d2f4336087aa32af945364870aba6720521
GabrielVanzo <gabriel.vanzo98@gmail.com>
2022-08-23 18:36:52 -0300

    Conclusao 5

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio5.js
@@ -0,0 +1,15 @@
+// EXERCICIO 5
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+info.personagem += ' e Tio Patinhas';
+info.origem += " e Christmas on Bear Mountain, Dell's Four Color Comics #178"
+info.nota += ' e O último MacPatinhas'
+info.recorrente = 'Sim'
+
+for (let key in info) {
+    console.log(info[key]);
+}
