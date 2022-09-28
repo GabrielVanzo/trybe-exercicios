5251a01f2013fed6da03e9f35ca68d778ab6b631
GabrielVanzo <gabriel.vanzo98@gmail.com>
2022-09-28 11:38:55 -0300

    Conclusao exercicios dia 8.3

--- fundamentos/secao-8-higher-order-functions/dia-3-hofs-map-e-filter/exercise.js
+++ fundamentos/secao-8-higher-order-functions/dia-3-hofs-map-e-filter/exercise.js
@@ -117,4 +117,104 @@ function nameAndAge(array) {
       })).sort((a,b) => a.age - b.age)
 
 }
-console.log(nameAndAge(books));
+// console.log(nameAndAge(books));
+
+// Exercicio 3
+
+const expectedResult3 = [
+  { 
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: { name: 'George R. R. Martin', birthYear: 1948 },
+    releaseYear: 1991
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: { name: 'Isaac Asimov', birthYear: 1920 },
+    releaseYear: 1951
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: { name: 'Frank Herbert', birthYear: 1920 },
+    releaseYear: 1965
+  }
+];
+
+const fantasyOrScienceFiction = (array) => array.filter((element) => element.genre === 'Fantasia' || element.genre === 'Ficção Científica');
+// console.log(fantasyOrScienceFiction(books));
+
+// Exercicio 4
+
+const expectedResult4 = [
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+    releaseYear: 1928,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: { name: 'Isaac Asimov', birthYear: 1920 },
+    releaseYear: 1951,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954,
+  },
+];
+
+const oldBooksOrdered = (array) => array.filter((element) => (2022 - element.releaseYear) >= 60).sort((a, b) => a.releaseYear - b.releaseYear)
+// console.log(oldBooksOrdered(books));
+
+// Exercicio 5
+
+const expectedResult5 = [
+  'Frank Herbert',
+  'George R. R. Martin',
+  'Isaac Asimov',
+  'J. R. R. Tolkien',
+];
+
+function fantasyOrScienceFictionAuthors(array) {
+  const wantedGenres = ['Fantasia', 'Ficção Científica'];
+  return array.filter((element) => element.genre === 'Fantasia' || element.genre === 'Ficção Científica').map((element) => element.author.name).sort();
+};
+// console.log(fantasyOrScienceFictionAuthors(books));
+
+// Exercicio 6
+
+const expectedResult6 = [
+  'O Senhor dos Anéis',
+  'Fundação',
+  'O Chamado de Cthulhu',
+];
+
+function oldBooks(array) {
+  return array.filter((element) => (2022 - element.releaseYear) >= 60).map((element) => element.name)
+}
+// console.log(oldBooks(books));
+
+//  Exercicio 7
+
+const expectedResult7 = 'O Senhor dos Anéis';
+
+const authorWith3DotsOnName = (array) => array.find((element) => (element.author.name.split(' ').filter((iniciais) => iniciais.endsWith('.')).length === 3)).name
+console.log(authorWith3DotsOnName(books));
